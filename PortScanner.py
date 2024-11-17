92e4f66580dbabe507fdb6310125df7dbc76b0f3
Kaleth <K4ledcorcho@gmail.com>
2024-11-17 06:31:39 -0500

    PortScanner
    Esta herramienta esta escrita en python3 y permite realizar un rápido escaneo de puertos a cualquier host/IP a través de la herramienta Nmap.
    Para ejecutar el programa se debe contar con la librería de Nmap para Python
    comando: pip install python-nmap

---
+++ PortScanner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import nmap
+#print (Name program) K
+print()
+print("##  ###    ##     ####     ### ###  #### ##  ###  ##")
+print("##  ##      ##     ##       ##  ##  # ## ##   ##  ##")
+print("## ##     ## ##    ##       ##        ##      ##  ##")
+print("## ##     ##  ##   ##       ## ##     ##      ## ###")
+print("## ###    ## ###   ##       ##        ##      ##  ##")
+print("##  ##    ##  ##   ##  ##   ##  ##    ##      ##  ##")
+print("##  ###  ###  ##  ### ###  ### ###   ####    ###  ##")
+print("##  ###  ###  ##  ### ###  ### ###   ####    ###  ##")
+print()
+
+
+print("[Info] Herramienta para escanear los puertos abiertos en una dirección IP")
+print("  ||   Escrito en Python y utiliza Nmap")
+
+#print (Llama utilidad) K
+
+ip=input("[+] IP Objetivo ==> ")
+nm = nmap.PortScanner()
+puertos_abiertos="-p "
+results = nm.scan(hosts=ip,arguments="-sT -n -Pn -T4")
+count=0
+#print (results) K
+print("\nHost : %s" % ip)
+print("State : %s" % nm[ip].state())
+for proto in nm[ip].all_protocols():
+	print("Protocol : %s" % proto)
+	print()
+	lport = nm[ip][proto].keys()
+	sorted(lport)
+	for port in lport:
+		print ("port : %s\tstate : %s" % (port, nm[ip][proto][port]["state"]))
+		if count==0:
+			puertos_abiertos=puertos_abiertos+str(port)
+			count=1
+		else:
+			puertos_abiertos=puertos_abiertos+","+str(port)
+
+print("\nPuertos abiertos: "+ puertos_abiertos +" "+str(ip))
